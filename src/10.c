e6f45b68fb574f82e7bd88d1953ed016e27e697a
doppsoteros493 <doppsoteros493@gmail.com>
2025-03-15 18:41:33 +0000

    Add 10.c

---
+++ src/10.c
@@ -0,0 +1,8 @@
+
+int main(void) {
+    int age = 21;
+    char name[] = "John";
+    float gpa = 3.5;
+    printf("Hello, my name is %s and I am %d years old with a GPA of %f.\n", name, age, gpa);
+    return 0;
+}
